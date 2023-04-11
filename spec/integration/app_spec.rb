59fc7c8b6deb1417ff8365c2ca8983c5ef4660ea
djohn06 <danielajohn1406@gmail.com>
2023-04-11 17:39:27 +0100

    Updated to include tests for /login

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -3,29 +3,40 @@ require 'rack/test'
 require_relative "../../app"
 
 describe Application do
-    include Rack::Test::Methods
+  include Rack::Test::Methods
   
-    let(:app) { Application.new }
+  let(:app) { Application.new }
   
-    context "GET /new_message" do
-      it 'returns 200 OK' do
-        # Assuming the post with id 1 exists.
-        response = get('/new_message')
-  
-        expect(response.status).to eq(200)
-        expect(response.body).to include('<form method="POST" action="/new_message">')
-        expect(response.body).to include('<input type="text" name="message" />')
-      end
-  
-    context "Get /signup" do
-      it 'returns 200 OK' do
-        response = get('/signup')
-  
-        expect(response.status).to eq(200)
-        expect(response.body).to include('<input type="text" name="email" />')
-        expect(response.body).to include('<label>Password: </label>')
-        expect(response.body).to include('<input type="text" name="username" />')
-      end
+  context "GET /new_message" do
+    it 'returns 200 OK' do
+      # Assuming the post with id 1 exists.
+      response = get('/new_message')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include('<form method="POST" action="/new_message">')
+      expect(response.body).to include('<input type="text" name="message" />')
+    end
+  end
+
+  context "Get /signup" do
+    it 'returns 200 OK' do
+      response = get('/signup')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include('<input type="text" name="email" placeholder="Email" />')
+      expect(response.body).to include('<input type="text" name="password" placeholder="Password" />')
+      expect(response.body).to include('<input type="text" name="username" placeholder="Username" />')
+    end
+  end
+
+  context "Get /login" do
+    it 'returns 200 OK' do
+      response = get('/login')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include('<input type="text" name="username" placeholder="username"/>')
+      expect(response.body).to include('<input type="password" name="password" placeholder="password"/>')
+      expect(response.body).to include('<input type="submit" value="Login"/>')
     end
   end
 end
